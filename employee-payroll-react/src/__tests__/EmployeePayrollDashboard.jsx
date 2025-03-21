beb1653ae3694d7cc511f05d70bde7693deef362
Preu Sharma <preu1061.be21@chitkara.edu.in>
2025-03-21 11:10:04 +0530

    [Preu]:Added:added test case for dashboard/navbar (tdd approach)

--- employee-payroll-react/src/__tests__/EmployeePayrollDashboard.jsx
+++ employee-payroll-react/src/__tests__/EmployeePayrollDashboard.jsx
@@ -3,7 +3,19 @@ import EmployeePayrollDashboard from "../components/EmployeePayrollDashboard/Emp
 
 test('render dashboard container', () => {
     render(<EmployeePayrollDashboard/>);
+
     const element=screen.getByText(/Dashboard container/i);
     expect(element).toBeInTheDocument();
-  
 })
+
+
+test('render navbar container', () => {
+    render(<Navbar/>);
+
+    const logoImage = screen.getByAltText("Company Logo");
+    expect(logoImage).toBeInTheDocument();
+
+    const element=screen.getByText(/EMPLOYEE PAYROLL/i);
+    expect(element).toBeInTheDocument();
+})
+
